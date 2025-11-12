d9ad023d557887a972ad06f774f9ce7469fc9b96
cchome2024 <cchome2002@Hotmail.com>
2025-11-12 22:31:47 +0800

    Remove generated artifacts from repo

--- frontend/metro-transformer.js
+++ frontend/metro-transformer.js
@@ -1,18 +1,20 @@
 const upstreamTransformer = require('@expo/metro-config/babel-transformer');
 
-module.exports.transform = function ({ src, filename, options }) {
-  // 替换 import.meta 为兼容的代码
-  if (src.includes('import.meta')) {
-    src = src.replace(
-      /import\.meta\.url/g,
-      'typeof document !== "undefined" && document.currentScript ? document.currentScript.src : ""'
-    );
-    src = src.replace(
-      /import\.meta/g,
-      '{ url: typeof document !== "undefined" && document.currentScript ? document.currentScript.src : "" }'
-    );
+module.exports = {
+  transform: function ({ src, filename, options }) {
+    // 替换 import.meta 为兼容的代码
+    if (src.includes('import.meta')) {
+      src = src.replace(
+        /import\.meta\.url/g,
+        'typeof document !== "undefined" && document.currentScript ? document.currentScript.src : ""'
+      );
+      src = src.replace(
+        /import\.meta/g,
+        '{ url: typeof document !== "undefined" && document.currentScript ? document.currentScript.src : "" }'
+      );
+    }
+    
+    return upstreamTransformer.transform({ src, filename, options });
   }
-  
-  return upstreamTransformer.transform({ src, filename, options });
 };
 
